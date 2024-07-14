679357893949c6581c65943ac7eb96bbdffb0151
mattim0use <mattimose@gmail.com>
2024-07-14 13:20:52 -0700

    fixed stuff css mostly

--- packages/svelte/src/lib/components/Header.svelte
+++ packages/svelte/src/lib/components/Header.svelte
@@ -1,80 +1,169 @@
 <script lang="ts">
+
   import { page } from "$app/stores";
+
   import ConnectButton from "$lib/components/scaffold-eth/connect-button/ConnectButton.svelte";
+
   import { derived } from "svelte/store";
 
+
   type HeaderMenuLink = {
+
     label: string;
+
     href: string;
+
   };
 
+
   export const menuLinks: HeaderMenuLink[] = [
+
     {
+
       label: "Home",
       href: "/",
+
     },
+
     {
+
       label: "About",
+
       href: "/about",
+
     },
+
   ];
 
   const isCurrentPage = derived(page, $page => (href: string) => {
     return href === $page.url.pathname;
+
   });
+
 </script>
 
+
 <header class="header">
-  <div class="wallet-section">
+
+  <div class="header-content">
+
     <ConnectButton />
+
+    <div class="brand title_font svelte-bioemj">BANK OF BASED</div>
+
   </div>
-<div class="brand title_font">BANK OF BASED</div>
+
 </header>
 
 <style>
+
   @font-face {
+
     font-family: 'CloisterBlack';
+
     src: url('/fonts/CloisterBlack.ttf') format('truetype');
+
     font-weight: normal;
+
     font-style: normal;
+
   }
 
+
   .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+
+    position: relative;
     padding: 1rem;
+
     background-color: var(--background-color);
+
+  }
+
+
+  .header-content {
+
+    display: flex;
+
+    justify-content: space-between;
+
+    align-items: center;
   }
 
   .brand {
+
+    margin-left: auto; /* Pushes the brand to the right */
+
     font-family: 'CloisterBlack', sans-serif;
+
     font-size: 1.5rem;
+
   }
+
+
+  .header::after {
+
+    content: "";
+
+    position: absolute;
+
+    bottom: 0;
+
+    left: 0;
+
+    width: 100%;
+
+    height: 2px;
+
+    background-color: rgb(90, 90, 90); /* Border color */
+
+  }
+
 
   .nav {
+
     display: flex;
+
     gap: 1rem;
+
   }
+
 
   .nav a {
+
     text-decoration: none;
+
     color: var(--text-color);
+
   }
+
 
   .nav a.active {
+
     font-weight: bold;
+
   }
 
-  .wallet-section {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-  }
 
   .balance {
+
     margin-top: 0.5rem;
+
     font-size: 0.875rem;
+
     color: var(--text-color);
+
   }
+
+
+  .svelte-bioemj {
+
+    font-size: xx-large;
+
+    text-align: right;
+
+    color: #313131;
+
+
+  }
+
 </style>
